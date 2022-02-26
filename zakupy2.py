072a15941528187459ee10707c28f60a2dc51474
Grzegorz Gojke <ggojke@gmail.com>
2022-02-26 10:23:33 +0100

    Add zakupy2.py

--- zakupy2.py
+++ zakupy2.py
@@ -3,5 +3,25 @@ zakupy_dict = {"piekarnia": ["chleb", "bułki", "pączek"], "warzywniak": ["marc
 zakupy_v = []
 for k, v in zakupy_dict.items():
     print("Idę do", (k.capitalize()), ", kupuję tu następujące rzeczy:", (v))
+    print("Idę do {} , kupię tu następujące rzeczy:{}" .format(k, v))
     zakupy_v.append(len(v))
 print("W sumie kupie", sum(zakupy_v), "produktów")
+#
+print()
+#
+lista = [1, 2, 3, 5, 6, 11, 12, 18, 19, 21]
+liczby_pierwsze = []
+
+for num in lista:
+  is_prime=True
+  
+  if num > 2:
+    for i in range(2, num):
+      if num%i==0:
+        is_prime=False
+        break
+  
+  if is_prime:
+    liczby_pierwsze.append(num)
+
+print(liczby_pierwsze)
