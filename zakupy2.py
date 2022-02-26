e348739cf60f9c695d708793d7bb0c6f7dc788f6
Grzegorz Gojke <ggojke@gmail.com>
2022-02-26 11:08:05 +0100

    drugi

--- zakupy2.py
+++ zakupy2.py
@@ -6,3 +6,22 @@ for k, v in zakupy_dict.items():
     print("Idę do {} , kupię tu następujące rzeczy:{}" .format(k, v))
     zakupy_v.append(len(v))
 print("W sumie kupie", sum(zakupy_v), "produktów")
+#
+print()
+#
+lista = [1, 2, 3, 5, 6, 11, 12, 18, 19, 21]
+liczby_pierwsze = []
+
+for num in lista:
+  is_prime=True
+  
+  if num > 2:
+    for i in range(2, num):
+      if num%i==0:
+        is_prime=False
+        break
+  
+  if is_prime:
+    liczby_pierwsze.append(num)
+
+print(liczby_pierwsze)
